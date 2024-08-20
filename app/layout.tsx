21e12434e06f395ed7f4458345d0a74595fdd351
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-08-20 21:34:47 +0430

    init / startup code

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import StarsCanvas from "@/components/main/StarBackground";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Portfolio",
+  description: "This is my Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} bg-[#011026] overflow-y-scroll overflow-x-hidden`}
+      >
+        <StarsCanvas />
+        {children}
+        <SpeedInsights />
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="w-full h-full">
+      <div className="flex flex-col gap-20"></div>
+    </main>
+  );
+}
