2feaba9c28cb29662b32ccb729cad05d86ff0465
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-08-21 21:36:08 +0430

    more easier to manage

--- components/sections/skills.tsx
+++ components/sections/skills.tsx
@@ -8,44 +8,12 @@ import {
   slideInFromTop,
 } from "@/utils/motion";
 import { SparklesIcon } from "@heroicons/react/24/solid";
-import Image from "next/image";
-import { useInView } from "react-intersection-observer";
+
 import {
   Frontend_skills,
   Backend_skills,
 } from "@/components/contents/global-data";
-
-interface Props {
-  src: string;
-  width: number;
-  height: number;
-  index: number;
-}
-
-const SkillDataProvider = ({ src, width, height, index }: Props) => {
-  const { ref, inView } = useInView({
-    triggerOnce: true,
-  });
-
-  const imageVariants = {
-    hidden: { opacity: 0 },
-    visible: { opacity: 1 },
-  };
-
-  const animationDelay = 0.3;
-  return (
-    <motion.div
-      ref={ref}
-      initial="hidden"
-      variants={imageVariants}
-      animate={inView ? "visible" : "hidden"}
-      custom={index}
-      transition={{ delay: index * animationDelay }}
-    >
-      <Image src={src} width={width} height={height} alt="skill image" />
-    </motion.div>
-  );
-};
+import SkillDataProvider from "@/components/SkillDataProvider";
 
 const SkillsSection = () => {
   return (
