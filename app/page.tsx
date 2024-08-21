9367caf5a2f499a46fc9b074f0c3f6f45da25ff1
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-08-21 20:11:55 +0430

    some little modifications / added skills section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/sections/hero";
+import SkillsSection from "@/components/sections/skills";
 
 export default function Home() {
   return (
     <main className="w-full h-full">
       <div className="flex flex-col gap-20">
         <HeroSection />
+        <SkillsSection />
       </div>
     </main>
   );
---
+++ components/sections/skills.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+const SkillsSection = () => {
+  return (
+    <>
+      <h1>MY Skills</h1>
+    </>
+  );
+};
+
+export default SkillsSection;
