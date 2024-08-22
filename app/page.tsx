b6f44189e40a293b0488c9f44b05f90245e37321
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-08-22 09:52:46 +0430

    added footer section and some more

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 import HeroSection from "@/components/sections/hero";
 import SkillsSection from "@/components/sections/skills";
 import EncryptionSection from "@/components/sections/encryption";
+import Footer from "@/components/sections/footer";
+
 
 export default function Home() {
   return (
-    <main className="w-full h-full">
-      <div className="flex flex-col gap-20">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
         <HeroSection />
         <SkillsSection />
         <EncryptionSection />
+        <Footer />
       </div>
     </main>
   );
