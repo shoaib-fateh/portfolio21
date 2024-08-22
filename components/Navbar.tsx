33d9edb914cfbf2092ff909dd8fe792ac6628151
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-08-22 21:46:55 +0430

    some updates

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -42,7 +42,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
+    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-[9999999999] px-10">
       <div className="w-[100%] h-full flex flex-row items-center justify-between  m-auto px-[10px] gap-6">
         <a
           href="#about-me"
--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
 interface Props {
   src: string;
@@ -20,15 +20,17 @@ const ProjectCard = ({ src, title, description, onReadMoreClick }: Props) => {
       />
 
       <div className=" pt-[-16px]  h-[50%] px-3 md:h-[40%] my-[3%]">
-        <h1 className="text-2xl font-semibold text-white text-center">{title}</h1>
-        <p className="mt-2 md:text-[15px] max-sm:text-[13px]  text-gray-300">{description}</p>
+        <h1 className="text-2xl font-semibold text-white text-center">
+          {title}
+        </h1>
+        <p className="mt-2 md:text-[15px] max-sm:text-[13px]  text-gray-300">
+          {description}
+        </p>
       </div>
-      <button
-        type="button"
-        onClick={onReadMoreClick}
-        className="bottom-3 h-[40px] w-[150px] button-primary text-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 text-white cursor-pointer rounded-xl items-center"
-      >
-        read more
+
+      <button className="border font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
+        <span>read more</span>
+        <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
       </button>
     </div>
   );
