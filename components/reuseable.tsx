14c288a260747a625716e2a7904622075028c4db
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-08-24 13:51:55 +0430

    updated key attr

--- components/reuseable.tsx
+++ components/reuseable.tsx
@@ -4,11 +4,8 @@ import Image from "next/image";
 export const SocialMedia = () => (
   <>
     {socialMedia.map((info) => (
-      <a href={info.url}>
-        <div
-          key={info.id}
-          className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
-        >
+      <a href={info.url} key={info.id}>
+        <div className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
           <Image src={info.img} alt="icons" width={20} height={20} />
         </div>
       </a>
