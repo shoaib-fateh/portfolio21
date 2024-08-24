ff5986b3e5adc4c672b1f8fdfadabcceed176bf5
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-08-24 13:47:55 +0430

    replace img tag with nextjs Image

--- components/reuseable.tsx
+++ components/reuseable.tsx
@@ -1,4 +1,5 @@
 import { socialMedia } from "@/data";
+import Image from "next/image";
 
 export const SocialMedia = () => (
   <>
@@ -8,7 +9,7 @@ export const SocialMedia = () => (
           key={info.id}
           className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
         >
-          <img src={info.img} alt="icons" width={20} height={20} />
+          <Image src={info.img} alt="icons" width={20} height={20} />
         </div>
       </a>
     ))}
